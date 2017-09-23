6a303a8507b70ee8e7ee1ea645ff3d9b897c7540
Ted Watson <tedwatson123@mac.com>
2017-09-23 12:26:02 -0400

    Added buffer between spawning polos and play area bounds

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -5,6 +5,7 @@ using Valve.VR;
 
 public class gameController : MonoBehaviour {
 
+    public float wallBuffer = 2f;
     public GameObject polo;
     public GameObject head;
     public SteamVR_PlayArea playArea;
@@ -18,16 +19,16 @@ public class gameController : MonoBehaviour {
     {
         while (true)
         {
-            yield return new WaitForSeconds(3);
+            yield return new WaitForSeconds(.1f);
             SpawnPolo();
         }
     }
 
     private void SpawnPolo()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(playArea.vertices[1].x, playArea.vertices[0].x),
+        Vector3 spawnPosition = new Vector3(Random.Range(playArea.vertices[1].x + wallBuffer, playArea.vertices[0].x - wallBuffer),
                                             head.transform.position.y,
-                                            Random.Range(playArea.vertices[1].z, playArea.vertices[2].z));
+                                            Random.Range(playArea.vertices[1].z + wallBuffer, playArea.vertices[2].z - wallBuffer));
         Instantiate(polo, spawnPosition, Quaternion.identity);
     }
 }
