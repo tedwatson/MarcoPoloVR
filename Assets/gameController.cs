c3c43243680c388b698a3f8fd1906bedb9f53e1f
Ted Watson <tedwatson123@mac.com>
2017-09-23 12:55:50 -0400

    Prevented polo from spawning too close to player

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -5,13 +5,21 @@ using Valve.VR;
 
 public class gameController : MonoBehaviour {
 
-    public float wallBuffer = 2f;
+    public float wallBuffer = .4f;
+    public float minPlayerDistance = .5f;
     public GameObject polo;
     public GameObject head;
     public SteamVR_PlayArea playArea;
 
     private void Start()
     {
+        // Make sure our minPlayerDistance isn't set impossibly low
+        float maxPossibleSpawnDistance = MaxPossibleSpawnDistance();
+        if (minPlayerDistance > maxPossibleSpawnDistance)
+        {
+            minPlayerDistance = maxPossibleSpawnDistance;
+        }
+
         StartCoroutine(SpawnPolos());
     }
 
@@ -26,9 +34,43 @@ public class gameController : MonoBehaviour {
 
     private void SpawnPolo()
     {
-        Vector3 spawnPosition = new Vector3(Random.Range(playArea.vertices[1].x + wallBuffer, playArea.vertices[0].x - wallBuffer),
-                                            head.transform.position.y,
-                                            Random.Range(playArea.vertices[1].z + wallBuffer, playArea.vertices[2].z - wallBuffer));
+        bool foundGoodPosition = false;
+        Vector3 spawnPosition = Vector3.zero;
+        while (!foundGoodPosition)
+        {
+            // Pick a random eye-level position within our play area, but not too close to the edges
+            spawnPosition = new Vector3(Random.Range(playArea.vertices[1].x + wallBuffer, playArea.vertices[0].x - wallBuffer),
+                                                head.transform.position.y,
+                                                Random.Range(playArea.vertices[1].z + wallBuffer, playArea.vertices[2].z - wallBuffer));
+            // Make sure it's not too close to the player
+            if (DistanceIgnoringVertical(spawnPosition, head.transform.position) > minPlayerDistance)
+            {
+                foundGoodPosition = true;
+            }
+        }
         Instantiate(polo, spawnPosition, Quaternion.identity);
+    }
+    
+
+    private float DistanceIgnoringVertical(Vector3 p1, Vector3 p2)
+    {
+        return Vector3.Distance(new Vector3(p1.x, 0, p1.z), 
+                                new Vector3(p2.x, 0, p2.z));
+    }
+
+    private float MaxPossibleSpawnDistance()
+    {
+        // Return the maximum distance polo could spawn, assuming marco is standing in center of play area
+        return Vector3.Distance(new Vector3(playArea.vertices[1].x + wallBuffer,
+                                            0,
+                                            playArea.vertices[1].z + wallBuffer),
+                                new Vector3(average(playArea.vertices[1].x, playArea.vertices[0].x),
+                                            0,
+                                            average(playArea.vertices[1].z, playArea.vertices[2].z)));
+    }
+
+    private float average(float a, float b)
+    {
+        return (a + b) / 2f;
     }
 }
