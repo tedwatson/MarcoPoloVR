e9874cc86513b71296350d8b20edc9ea3371d812
Ted Watson <tedwatson123@mac.com>
2017-09-23 11:06:52 -0400

    Created Speech Recognition Test

---
+++ Assets/myScripts/speechRecognition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+using System.Collections.Generic;
+
+public class speechRecognition : MonoBehaviour {
+
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    private void Start()
+    {
+        print("Start");
+        // initialize
+        keywords.Add("marco", () =>
+        {
+            MarcoCalled();
+        });
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizerOnPhraseRecognized;
+        keywordRecognizer.Start();
+    }
+
+    void KeywordRecognizerOnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        print("KeywordRecognizerOnPhraseRecognized");
+        System.Action keywordAction;
+
+        if(keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+
+    void MarcoCalled()
+    {
+        print("You just said MARCO");
+    }
+
+}
