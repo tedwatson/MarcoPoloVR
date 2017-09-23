f0b127f4c8226fd9be6f6a2aa7732c2252264fa3
Ted Watson <tedwatson123@mac.com>
2017-09-23 12:19:42 -0400

    Implemented Randomly Spawning Point within Play Area

---
+++ Assets/gameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class gameController : MonoBehaviour {
+
+    public GameObject polo;
+    public GameObject head;
+    public SteamVR_PlayArea playArea;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnPolos());
+    }
+
+    IEnumerator SpawnPolos()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3);
+            SpawnPolo();
+        }
+    }
+
+    private void SpawnPolo()
+    {
+        Vector3 spawnPosition = new Vector3(Random.Range(playArea.vertices[1].x, playArea.vertices[0].x),
+                                            head.transform.position.y,
+                                            Random.Range(playArea.vertices[1].z, playArea.vertices[2].z));
+        Instantiate(polo, spawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/playAreaTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class playAreaTest : MonoBehaviour {
+
+    private SteamVR_PlayArea playArea;
+
+	// Use this for initialization
+	void Start () {
+        playArea = GetComponent<SteamVR_PlayArea>();
+        for (int i = 0; i < playArea.vertices.Length; i++)
+        {
+            print("element " + i + " is " + playArea.vertices[i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
